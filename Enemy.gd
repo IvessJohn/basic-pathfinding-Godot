d08f05970a46c8d8f65a519dfbbe6b91def69f65
IvessJohn <59248545+IvessJohn@users.noreply.github.com>
2021-04-11 06:23:46 +0300

    Episode 1
    Episode 1: Added a functioning pathfinding itself

--- Enemy.gd
+++ Enemy.gd
@@ -3,9 +3,40 @@ extends KinematicBody2D
 export(int) var SPEED: int = 40
 var velocity: Vector2 = Vector2.ZERO
 
+var path: Array = []	# Contains destination positions
+var levelNavigation: Navigation2D = null
+var player = null
+
+onready var line2d = $Line2D
+
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	var tree = get_tree()
+	if tree.has_group("LevelNavigation"):
+		levelNavigation = tree.get_nodes_in_group("LevelNavigation")[0]
+	if tree.has_group("Player"):
+		player = tree.get_nodes_in_group("Player")[0]
 
 func _physics_process(delta):
+	line2d.global_position = Vector2.ZERO
+	if player:
+		generate_path()
+		navigate()
 	move()
+
+func navigate():	# Define the next position to go to
+	if path.size() > 0:
+		velocity = global_position.direction_to(path[1]) * SPEED
+		
+		# If reached the destination, remove this point from path array
+		if global_position == path[0]:
+			path.pop_front()
+
+func generate_path():	# It's obvious
+	if levelNavigation != null and player != null:
+		path = levelNavigation.get_simple_path(global_position, player.global_position, false)
+		line2d.points = path
 
 func move():
 	velocity = move_and_slide(velocity)
